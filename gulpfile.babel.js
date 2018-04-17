acf6cb666f34ceb124430553880f00d4d86c9730
yorickov <yorickov@gmail.com>
2018-04-17 16:53:39 +0300

    add travis climat

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,10 +2,8 @@ import 'babel-polyfill';
 
 import gulp from 'gulp';
 import getServer from '.';
-import log from './lib/logger';
 
 gulp.task('server', () => {
-  log('hi');
   getServer().listen(process.env.PORT || 3000, () =>
     console.log('server started'));
 });
