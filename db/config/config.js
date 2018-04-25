fa1e53fb4fa46ebda012d050e539a4e4ac4f89a9
yorickov <yorickov@gmail.com>
2018-04-25 04:59:46 +0300

    fix

--- db/config/config.js
+++ db/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
     dialect: 'sqlite',
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: DATABASE_URL,
     dialect: 'postgres',
   },
 };
